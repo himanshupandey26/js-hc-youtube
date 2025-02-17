872dfde67f6b1b15e569812a4996be9eb5b6ecb3
Himanshu Pandey <142781739+himanshupandey26@users.noreply.github.com>
2025-02-17 19:17:34 +0000

    Datatype

---
+++ O1_Basics/O2_Datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // Don't use this type, Aim for code readability should be high
+
+console.log("Himanshu")
+
+
+let name = "Himanshu"
+let age = 20
+let isLoggedIn = false
+let state;
+
+console.log(typeof age)
+
+
+// number => 2 to power 53
+// bigint => Very big value, used in stock market, big MNC data
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique show karne ke liye
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
