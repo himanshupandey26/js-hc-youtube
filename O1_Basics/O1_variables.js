db7f6d20963004be661044811f6a5ec6bdbbb57a
Himanshu Pandey <142781739+himanshupandey26@users.noreply.github.com>
2025-02-17 18:55:11 +0000

    Variables

---
+++ O1_Basics/O1_variables.js
@@ -0,0 +1,22 @@
+const accountID = 323232
+let accountName = "Himanshu"
+var accountPassword = 1234
+accountCity = "Dilema"
+
+let accountState;
+
+//accountID = 343434
+// Assignment to constant variable is not allowed
+
+/*
+Var keyword is not prefer to use
+because of issue in block scope and functional scope
+*/
+
+accountName = "Himanshu Pandey"
+accountPassword = "21212121"
+accountCity = "Sambala"
+
+
+console.log(accountID);
+console.table([accountID, accountName, accountPassword, accountCity, accountState])
