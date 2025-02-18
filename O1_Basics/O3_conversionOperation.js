11c3fc047fa01f79fe1ec4dc52f11e0100f4b950
himanshupandey26 <himanshupandey7860988150@gmail.com>
2025-02-18 23:49:14 +0530

    Conversions

---
+++ O1_Basics/O3_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "Himanshu";
+// console.log(typeof score);
+let scoreNumber = Number(score);
+// console.log(typeof(scoreNumber));
+// console.log(scoreNumber);
+
+// "" => 0
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+//  null => 0
+// undefined => NaN
+
+
+let isLoggedIn = 0.0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 32 => true
+// 1 => true; 0 => false
+// 0.0=> false
+// "" => false
+// null => false
+// undefined => false
+// "hitesh" => true
+
+
+let someValue = 34
+let stringValue = String(someValue)
+// console.log(stringValue);
+// console.log(typeof stringValue);
+
+
+
