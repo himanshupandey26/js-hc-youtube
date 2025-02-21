aeefb211f907d11c6e1ac81c1b668d8aacad4b36
himanshupandey26 <himanshupandey7860988150@gmail.com>
2025-02-21 13:13:37 +0530

    add more info

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,22 +2,22 @@
 function sayMyName(){
     console.log("H");
     console.log("I");
-    console.log("T");
-    console.log("E");
+    console.log("M");
+    console.log("A");
+    console.log("N");
     console.log("S");
     console.log("H");
+    console.log("U");
 }
 
-// sayMyName is a reference and sayMyName() is execution
-// sayMyName()
+// sayMyName()   // sayMyName is a reference and sayMyName() is execution
+
 
 // function addTwoNumbers(number1, number2){
-
 //     console.log(number1 + number2);
 // }
 
 function addTwoNumbers(number1, number2){
-
     // let result = number1 + number2
     // return result
     return number1 + number2
@@ -27,10 +27,10 @@ const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
 
-
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
+// function loginUserMessage(username){
+function loginUserMessage(username = "sam"){ // default value
+    if(!username){ //(username === undefined) bhi use kar skte h
+        console.log("Please enter a username");
         return
     }
     return `${username} just logged in`
@@ -39,15 +39,17 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
 
-
-function calculateCartPrice(val1, val2, ...num1){
+// ... is rest operator or spread operator based on their use case which is used (rest/spread)
+// function calculateCartPrice(...num1){ // ... is rest operator, return array of all argument pass
+//     return num1
+// }
+function calculateCartPrice(val1, val2, ...num1){ // ... is rest operator, return array of  all  argument pass starting from 3 argument, since 1 arg to val1 and 2 arg to val2
     return num1
-}
-
+} 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
 const user = {
-    username: "hitesh",
+    username: "himanshu",
     prices: 199
 }
 
