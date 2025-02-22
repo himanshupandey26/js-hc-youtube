c20d24450f3f8202ef6d62684f692292ef095d48
himanshupandey26 <himanshupandey7860988150@gmail.com>
2025-02-22 10:00:05 +0530

    for of and map

---
+++ 05_iterations/03_three.js
@@ -0,0 +1,58 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+// Map(3) {
+//   'IN' => 'India',
+//   'USA' => 'United States of America',
+//   'Fr' => 'France'
+// }
+
+for (const val of map) {
+  console.log(val);
+}
+// [ 'IN', 'India' ]
+// [ 'USA', 'United States of America' ]
+// [ 'Fr', 'France' ]
+
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+// IN :- India
+// USA :- United States of America
+// Fr :- France
+
+
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) { // NOT work, myObject is not iterable
+//     console.log(key, ':-', value);
+    
+// }
